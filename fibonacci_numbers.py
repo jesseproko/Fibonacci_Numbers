c1d548a2c74328f10b01cccc84ec670a74b214f2
jesseproko <jesseproko@gmail.com>
2024-07-15 14:27:19 -0700

    Added .py

---
+++ fibonacci_numbers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x = int(input("what number would you like to start with (>=1)? "))
+y = x
+i = 10
+print(x)
+for n in range(i):
+    x_old = x
+    print(x)
+    x = x+y
+    y = x_old
+
+
+# In[ ]:
+
+
+
+
